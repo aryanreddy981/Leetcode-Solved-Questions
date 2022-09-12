6cc303f463b5433993351f636b461cd8c95fce43
aryanreddy981 <109081283+aryanreddy981@users.noreply.github.com>
2022-09-12 20:02:53 +0530

    Time: 3 ms (80.54%), Space: 47.5 MB (75.43%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,23 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode a, ListNode b) {
+//         ListNode dummy=new ListNode(0);
+//         ListNode tail=dummy;
+//         int carry=0;
+//         while(a!=null || b!=null)
+//         {
+//             int x = (a!=null)?a.val:0;
+//             int y=(b!=null)?b.val:0;
+//             int sum=carry +x + y;
+//             carry=sum/10;
+//             tail.next=new ListNode(sum%10);
+//             tail=tail.next;
+//             if(a!=null) a=a.next;
+//             if(b!=null) b=b.next;
+//         }
+//         if(carry>0)
+//             tail.next=new ListNode(carry);
+//         return dummy.next;
+        
+//     }
+// }
         ListNode dummy=new ListNode(0);
-        ListNode tail=dummy;
+        ListNode curr=dummy;
         int carry=0;
-        while(a!=null || b!=null)
+        while(a != null || b != null)
         {
-            int x = (a!=null)?a.val:0;
-            int y=(b!=null)?b.val:0;
-            int sum=carry +x + y;
+            int x=a!=null?a.val:0;
+            int y=b!=null?b.val:0;
+            int sum=carry+ x + y;
             carry=sum/10;
-            tail.next=new ListNode(sum%10);
-            tail=tail.next;
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
             if(a!=null) a=a.next;
             if(b!=null) b=b.next;
         }
         if(carry>0)
-            tail.next=new ListNode(carry);
+            curr.next=new ListNode(carry);
         return dummy.next;
-        
     }
 }
