cbeaae48040e73aa9dcc6bff9ae711bdf3424f73
AryanReddy <109081283+aryanreddy981@users.noreply.github.com>
2022-10-17 10:34:17 +0530

    Time: 1 ms (50.19%), Space: 42 MB (70.30%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,18 +14,44 @@
  * }
  */
 //if ypu are not going to delete an element from linked list then declare arraylist otherwise linkedlist
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> list=new ArrayList<>();
+//         helper(root,list);
+//         return list;
+        
+//     }
+//     private void helper(TreeNode node,List<Integer> list){
+//         if(node==null)
+//             return;
+//         helper(node.left,list);
+//         helper(node.right,list);
+//         list.add(node.val);
+//     }
+// }
+
+
+
+
+//Iterative Approach
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        helper(root,list);
-        return list;
-        
+    LinkedList<Integer> ans=new LinkedList<>();
+        Stack<TreeNode> stack=new Stack<>();
+        if(root==null) return ans;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode cur=stack.pop();
+            ans.addFirst(cur.val);
+            if(cur.left!=null){
+                stack.push(cur.left);
+            }
+            if(cur.right!=null){
+                stack.push(cur.right);
+            }
+            }
+        return ans;
     }
-    private void helper(TreeNode node,List<Integer> list){
-        if(node==null)
-            return;
-        helper(node.left,list);
-        helper(node.right,list);
-        list.add(node.val);
-    }
-}
+            
+        }
+            
