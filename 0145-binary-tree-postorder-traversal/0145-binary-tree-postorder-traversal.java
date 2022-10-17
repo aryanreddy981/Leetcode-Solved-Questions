70040ed1824d2dd21cba4412260064b1bafc60b4
AryanReddy <109081283+aryanreddy981@users.noreply.github.com>
2022-10-17 10:24:59 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (63.97%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//if ypu are not going to delete an element from linked list then declare arraylist otherwise linkedlist
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        helper(root,list);
+        return list;
+        
+    }
+    private void helper(TreeNode node,List<Integer> list){
+        if(node==null)
+            return;
+        helper(node.left,list);
+        helper(node.right,list);
+        list.add(node.val);
+    }
+}
